acaca636ba39aa65f9d0869ef26a250f74e3f45e
J/A <archer884@gmail.com>
2021-01-27 19:54:56 -0600

    Reorganize

---
+++ src/lib.rs
@@ -0,0 +1,89 @@
+use rand::RngCore;
+
+pub struct SquirrelRng {
+    position: u32,
+    seed: u32,
+}
+
+impl SquirrelRng {
+    pub fn new() -> Self {
+        Self {
+            position: 0,
+            seed: rand::thread_rng().next_u32(),
+        }
+    }
+}
+
+impl RngCore for SquirrelRng {
+    #[inline]
+    fn next_u32(&mut self) -> u32 {
+        let result = squirrel3(self.position, self.seed);
+        self.position += 1;
+        result
+    }
+
+    #[inline]
+    fn next_u64(&mut self) -> u64 {
+        next_u64_via_u32(self)
+    }
+
+    #[inline]
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        fill_bytes_via_next(self, dest);
+    }
+
+    #[inline]
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand::Error> {
+        self.fill_bytes(dest);
+        Ok(())
+    }
+}
+
+pub fn squirrel3(position: u32, seed: u32) -> u32 {
+    const BIT_NOISE1: u32 = 0xB5297A4D;
+    const BIT_NOISE2: u32 = 0x68E31DA4;
+    const BIT_NOISE3: u32 = 0x1B56C4E9;
+
+    let mut mangled = position as u32;
+    mangled = mangled.wrapping_mul(BIT_NOISE1);
+    mangled = mangled.wrapping_add(seed);
+    mangled ^= mangled >> 8;
+    mangled = mangled.wrapping_add(BIT_NOISE2);
+    mangled ^= mangled << 8;
+    mangled = mangled.wrapping_mul(BIT_NOISE3);
+    mangled ^= mangled >> 8;
+    mangled
+}
+
+/// Implement `next_u64` via `next_u32`, little-endian order. Taken straight
+/// from rand.
+pub fn next_u64_via_u32<R: RngCore + ?Sized>(rng: &mut R) -> u64 {
+    // Use LE; we explicitly generate one value before the next.
+    let x = u64::from(rng.next_u32());
+    let y = u64::from(rng.next_u32());
+    (y << 32) | x
+}
+
+/// Implement `fill_bytes` via `next_u64` and `next_u32`, little-endian order.
+///
+/// The fastest way to fill a slice is usually to work as long as possible with
+/// integers. That is why this method mostly uses `next_u64`, and only when
+/// there are 4 or less bytes remaining at the end of the slice it uses
+/// `next_u32` once.
+fn fill_bytes_via_next<R: RngCore + ?Sized>(rng: &mut R, dest: &mut [u8]) {
+    let mut left = dest;
+    while left.len() >= 8 {
+        let (l, r) = { left }.split_at_mut(8);
+        left = r;
+        let chunk: [u8; 8] = rng.next_u64().to_le_bytes();
+        l.copy_from_slice(&chunk);
+    }
+    let n = left.len();
+    if n > 4 {
+        let chunk: [u8; 8] = rng.next_u64().to_le_bytes();
+        left.copy_from_slice(&chunk[..n]);
+    } else if n > 0 {
+        let chunk: [u8; 4] = rng.next_u32().to_le_bytes();
+        left.copy_from_slice(&chunk[..n]);
+    }
+}
